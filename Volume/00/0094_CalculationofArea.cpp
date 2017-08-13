c91e324aeebf4e56ab51c7b2ec3a34bb7dd51488
Struuuuggle <s15ti032@gmail.com>
2017-08-14 00:28:39 +0900

    Added 0094

---
+++ Volume/00/0094_CalculationofArea.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+using namespace std;
+int main() {
+  int a, b;
+  double tsubo;
+  scanf("%d %d", &a, &b);
+  tsubo = a * b / 3.305785;
+  printf("%.5f\n", tsubo);
+  return 0;
+}
