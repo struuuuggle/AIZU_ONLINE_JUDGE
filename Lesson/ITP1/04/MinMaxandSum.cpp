65642c7d6c0e767ff57da1583e0ea77591ed7d49
Struuuuggle <s15ti032@gmail.com>
2017-08-14 17:52:32 +0900

    Modified MinMaxandSum

--- Lesson/ITP1/04/MinMaxandSum.cpp
+++ Lesson/ITP1/04/MinMaxandSum.cpp
@@ -1,36 +1,18 @@
-/*
- *n個の整数 ai(i=1,2,...n)を入力し、それらの最小値、最大値、合計値を求めるプログラム
- *0<n≤10000
- *−1000000≤ai≤1000000
-*/
-
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
-
 int main() {
   int n;
-  long v, min, max;
-  long sum = 0;//int型だとaiが入りきらない可能性がある
-
+  long long sum = 0;
   cin >> n;
-  
-  for (int i = 0; i < n; i++) {
-	cin >> v;
-	sum += v;
-	if (i == 0) {
-	  max = v;
-	  min = v;
-	}
-	else {
-	  if (max <= v) {
-		max = v;
-	  }
-	  if (min >= v) {
-		min = v;
-	  }
-	}
+  vector<int> v1(n);
+  for(int i = 0; i < n; ++i) {
+	cin >> v1[i];
+	sum += v1[i];
   }
-  cout << min << " " << max << " " << sum << endl;
+  cout << *min_element(v1.begin(), v1.end()) << " ";
+  cout << *max_element(v1.begin(), v1.end()) << " ";
+  cout << sum << endl;
   return 0;
 }
