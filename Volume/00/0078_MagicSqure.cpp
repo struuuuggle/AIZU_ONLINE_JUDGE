46e5b3617f51c31312ddee38e7ec7453c05476da
Struuuuggle <s15ti032@gmail.com>
2017-08-29 02:30:22 +0900

    Modified 0078

--- Volume/00/0078_MagicSqure.cpp
+++ Volume/00/0078_MagicSqure.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <iomanip>
 using namespace std;
+
 void magicSquare(vector<vector<int> > &v1) {
   int n = v1.size();
-  int row = (n / 2 + 1), column = n / 2;
+  int r = (n / 2 + 1), c = n / 2;
   
-  v1[row][column] = 1;
+  v1[r][c] = 1;
   for(int cnt = 2; cnt <= n * n; ++cnt) {
-	row = (row == n - 1 ? 0 : row + 1);
-	column = (column == n - 1 ? 0 : column + 1);
+	r = (r == n - 1 ? 0 : r + 1);
+	c = (c == n - 1 ? 0 : c + 1);
 
-	while(v1[row][column] != 0) {
-	  row = (row == n - 1 ? 0 : row + 1);
-	  column = (column == 0 ? n - 1 : column - 1);
+	while(v1[r][c] != 0) {
+	  r = (r == n - 1 ? 0 : r + 1);
+	  c = (c == 0 ? n - 1 : c - 1);
 	}
-	v1[row][column] = cnt;
+	v1[r][c] = cnt;
   }
+}
+
+void printVector(vector<vector<int> > &v1) {
+  int n = v1.size();
+  for(int i = 0; i < n; ++i) {
+	  for(int j = 0; j < n; ++j) {
+		cout << right << setw(4) << v1[i][j];
+	  }
+	  cout << endl;
+	}
 }
 
 int main() {
@@ -27,12 +37,7 @@ int main() {
 
 	vector<vector<int> > v1(n, vector<int>(n, 0));
 	magicSquare(v1);
-	for(int i = 0; i < n; ++i) {
-	  for(int j = 0; j < n; ++j) {
-		cout << right << setw(4) << v1[i][j];
-	  }
-	  cout << endl;
-	}
+	printVector(v1);
   }
   return 0;
 }
