ec3c254189311b604902bfc059cd3be1b43d9633
struuuuggle <s15ti032@gmail.com>
2017-10-22 01:00:56 +0900

    Added 0013_SwitchingRailroadCars

---
+++ Volume/00/0013_SwitchingRailroadCars.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+  int n;
+  stack<int> st;
+  while(cin >> n) {
+    if(n == 0) {
+      cout << st.top() << endl;
+      st.pop();
+    }
+    else {
+      st.push(n);
+    }
+  }
+  return 0;
+}
